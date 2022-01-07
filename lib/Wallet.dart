b4aba4466292d443accd89bb6c7112d50bd481b8
Rahul Prakash <96944552+rahulPrakash02@users.noreply.github.com>
2022-01-07 15:45:33 +0530

    Update Wallet.dart

--- lib/Wallet.dart
+++ lib/Wallet.dart
@@ -1,6 +1,10 @@
+//Dart Class for the wallet
 class Wallet {
+  
+  //public object for balance
   double balance = 5000.0;
 
+  //function for withdrawing amount
   void withdraw(double amount) {
     balance -= amount;
   }
